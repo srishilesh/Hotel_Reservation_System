c764422806908d76648a9c899549fd410a628dc8
srishilesh <srishilesh@gmail.com>
2020-06-22 00:33:20 +0530

    Navbar components and Routing

--- resort/src/index.js
+++ resort/src/index.js
@@ -1,13 +1,15 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
 import './index.css';
+
+import {BrowserRouter as Router} from 'react-router-dom'; // Used for Routing - refer React Training
 import App from './App';
 import * as serviceWorker from './serviceWorker';
 
 ReactDOM.render(
-  <React.StrictMode>
+  <Router> 
     <App />
-  </React.StrictMode>,
+  </Router>,
   document.getElementById('root')
 );
 
